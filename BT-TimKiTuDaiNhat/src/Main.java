76ab100b4bfcbf069076c20fe05296322b6f97a8
omgbqh98 <69282402+omgbqh98@users.noreply.github.com>
2020-09-25 11:08:44 +0700

    dfg

---
+++ BT-TimKiTuDaiNhat/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input String: ");
+        String st = scanner.nextLine();
+
+        LinkedList<Character> max = new LinkedList<>();
+
+        for (int i = 0; i < st.length(); i++) {
+            LinkedList<Character> list = new LinkedList<>();
+            list.add(st.charAt(i));
+            for (int j = i + 1; j < st.length(); j++) {
+                if (st.charAt(j) > list.getLast()) {
+                    list.add(st.charAt(j));
+                }
+            }
+            if (list.size() > max.size()) {
+                max.clear();
+                max.addAll(list);
+            }
+            list.clear();
+        }
+        for (Character character : max) {
+            System.out.print(character);
+        }
+//        System.out.println();
+    }
+}
